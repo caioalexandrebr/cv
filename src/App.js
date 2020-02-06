0b5a9de1ec0db054bdaebdeee668ee65115fe4b6
Caio Alexandre <caioalexandre.br@gmail.com>
2020-02-06 14:07:28 -0300

    refactor(app): removing routing system

--- src/App.js
+++ src/App.js
@@ -4,41 +4,14 @@ import Curriculum from './pages/Curriculum';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
 
 const App = () => (
   <Router>
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/cv">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/users">Users</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
-
     <Switch>
       <Route exact path="/" component={Curriculum}/>
       <Route path="/cv" component={Curriculum}/>
-      <Route path="/about" component={About}/>
-      <Route path="/users" component={Users}/>
     </Switch>
   </Router>
 );
